386004aa9f2d445412037d4e7bdfefe63e5fa7b6
endorphinians <endorphinians@gmail.com>
2024-03-08 10:41:47 +0800

    - - Changes to be committed: -	new file:   .github/workflows/firebase-hosting-merge.yml -	new file:   .github/workflows/firebase-hosting-pull-request.yml -	new file:   Admin_panel/.firebaserc -	new file:   Admin_panel/.gitignore -	new file:   Admin_panel/database.rules.json -	modified:   Admin_panel/firebase.json -	new file:   Admin_panel/functions/.eslintrc.js -	new file:   Admin_panel/functions/.gitignore -	new file:   Admin_panel/functions/index.js -	new file:   Admin_panel/functions/package-lock.json -	new file:   Admin_panel/functions/package.json -	modified:   Admin_panel/node_modules/.package-lock.json -	modified:   Admin_panel/package-lock.json -	modified:   Admin_panel/package.json -	new file:   Admin_panel/public/404.html -	new file:   Admin_panel/public/index.html -	modified:   Admin_panel/src/environments/environment.prod.ts -	new file:   Admin_panel/storage.rules -	modified:   Admin_panel/tsconfig.json -	modified:   Admin_panel/yarn.lock -	new file:   oder/.editorconfig -	new file:   oder/.gitignore -	new file:   oder/.vscode/extensions.json -	new file:   oder/.vscode/launch.json -	new file:   oder/.vscode/tasks.json -	new file:   oder/README.md -	new file:   oder/angular.json -	new file:   oder/package-lock.json -	new file:   oder/package.json -	new file:   oder/src/app/app.component.css -	new file:   oder/src/app/app.component.html -	new file:   oder/src/app/app.component.spec.ts -	new file:   oder/src/app/app.component.ts -	new file:   oder/src/app/app.config.ts -	new file:   oder/src/app/app.routes.ts -	new file:   oder/src/assets/.gitkeep -	new file:   oder/src/favicon.ico -	new file:   oder/src/index.html -	new file:   oder/src/main.ts -	new file:   oder/src/styles.css -	new file:   oder/tsconfig.app.json -	new file:   oder/tsconfig.json -	new file:   oder/tsconfig.spec.json -

--- Admin_panel/src/environments/environment.prod.ts
+++ Admin_panel/src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
+    apiKey: "AIzaSyAGtW9TlX0l4SDmNyMv5EuZnbJ2FWP9OPQ",
+  authDomain: "foodie-5f111.firebaseapp.com",
+  databaseURL: "https://foodie-5f111-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "foodie-5f111",
+  storageBucket: "foodie-5f111.appspot.com",
+  messagingSenderId: "536453044515",
+  appId: "1:536453044515:web:026284da15f5de3d611496",
+  measurementId: "G-PTV4XEX2C8"
   },
   onesignal: {
     appId: '',
@@ -19,7 +19,7 @@ export const environment = {
     sk: ''
   },
   general: {
-    symbol: '$',
-    code: 'USD'
+    symbol: 'RM',
+    code: 'MYR'
   }
 };
