e00fdbf4346859e442b1d771012bf11bdaadcd94
endorphinians <endorphinians@gmail.com>
2024-04-27 13:41:26 +0800

    Changes to be committed: 	modified:   food-app-admin/angular.json 	modified:   food-app-admin/database.rules.json 	new file:   food-app-admin/package-dep-report-Admin_Panel.html 	modified:   food-app-admin/package-lock.json 	modified:   food-app-admin/package.json 	modified:   food-app-admin/src/environments/environment.ts 	modified:   food-app-admin/yarn.lock

--- food-app-admin/src/environments/environment.ts
+++ food-app-admin/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
+    apiKey: "AIzaSyAGtW9TlX0l4SDmNyMv5EuZnbJ2FWP9OPQ",
+  authDomain: "foodie-5f111.firebaseapp.com",
+  databaseURL: "https://foodie-5f111-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "foodie-5f111",
+  storageBucket: "foodie-5f111.appspot.com",
+  messagingSenderId: "536453044515",
+  appId: "1:536453044515:web:026284da15f5de3d611496",
+  measurementId: "G-PTV4XEX2C8"
   },
   onesignal: {
     appId: '',
